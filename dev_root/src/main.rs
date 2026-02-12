f3a2cbae8a56e5510aefd89824c7d0f42f7224bd
X Files <tavi006official@gmail.com>
2026-02-12 20:56:30 +0100

    cool text styles

--- dev_root/src/main.rs
+++ dev_root/src/main.rs
@@ -13,8 +13,34 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello from VGA!\n");
-    println!("Hello from VGA!\n");
-    println!("Hello from VGA!\n");
+    use crate::vga_buffer::{WRITER, Color, ColorCode};
+
+    {
+        let mut w = WRITER.lock();
+        w.color_code = ColorCode::new(Color::LightCyan, Color::Black);
+        w.write_string("=== TESTOS3 BOOT SEQUENCE ===\n\n");
+    }
+
+    {
+        let mut w = WRITER.lock();
+        w.color_code = ColorCode::new(Color::Yellow, Color::Black);
+        w.write_string("Hello World!! :3\n");
+        w.write_string("All in working order!\n");
+    }
+
+    {
+        let mut w = WRITER.lock();
+        w.color_code = ColorCode::new(Color::LightGreen, Color::Black);
+        w.write_string("Lorem ipsum dolor sit amet,\n");
+        w.write_string("consectetur adipiscing elit.\n");
+        w.write_string("Sed do eiusmod tempor incididunt ut labore.\n\n");
+    }
+
+    {
+        let mut w = WRITER.lock();
+        w.color_code = ColorCode::new(Color::Pink, Color::Black);
+        w.write_string(">> SYSTEM READY <<\n");
+    }
+
     loop {}
 }
